185ef4c5a8aeb77255688042e95d5a0ed859ae5b
najamulsaqib <1najamulsaqib@gmail.com>
2024-08-21 17:52:38 +0100

    Analytics Injection

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
+import { inject } from "@vercel/analytics";
 import { injectSpeedInsights } from "@vercel/speed-insights";
 import { defaultProps, targetElements } from "./data/scrollRevealConfig";
 import initScrollReveal from "./scripts/scrollReveal";
 import initTiltEffect from "./scripts/tiltAnimation";
 
 injectSpeedInsights();
+inject();
 initScrollReveal(targetElements, defaultProps);
 initTiltEffect();
